ecafcfab3a8b3c130fafed6dc58b159682ee1def
MINGUOLIU <66464677+MINGUOLIU@users.noreply.github.com>
2023-12-21 21:31:36 +0800

    leran #message

--- course/005.cmake
+++ course/005.cmake
@@ -1,3 +1,5 @@
 message(STATUS "PATH=$ENV{PATH}")
 set(MY_VAR "hello world" CACHE STRING "This is my variable")
 message(The value of MY_VAR is: ${MY_VAR})
+
+message(The value of MY_VAR is: ${MY_VAR})
